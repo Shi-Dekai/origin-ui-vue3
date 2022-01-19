86fb9ac88f14bcc3bed030a314f8f57a7f688947
Shidekai <1727259503@qq.com>
2022-01-19 16:12:24 +0800

    初始化 aside html

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -5,8 +5,8 @@
       <h1>OriginUI</h1>
       <h2>一个很厉害的 UI 框架</h2>
       <p class="actions">
-        <a href="">GitHub</a>
-        <a href="">开始</a>
+        <a href="https://github.com/Shi-Dekai">GitHub</a>
+        <router-link to="/doc">开始</router-link>
       </p>
     </div>
   </div>
