3ca0e7bc6f8760ac958658cbc90afcaa9da963e3
史德凯 <1727259503@qq.com>
2022-02-15 17:53:50 +0800

    加载中 css

--- src/components/ButtonDemo.vue
+++ src/components/ButtonDemo.vue
@@ -1,10 +1,11 @@
 <template>
+  <h1>示例2</h1>
   <section>
     <Button>按钮</Button>
     <Button theme="link">link按钮</Button>
     <Button theme="text">text按钮</Button>
   </section>
-  <p>示例2</p>
+  <h1>示例2</h1>
   <div>
     <div>
       <Button size="big">大大大</Button>
@@ -22,7 +23,7 @@
       <Button size="small" theme="text">小小小</Button>
     </div>
   </div>
-  <p>示例3</p>
+  <h1>示例3</h1>
   <div>
     <div>
       <Button level="main">主要按钮</Button>
@@ -40,11 +41,15 @@
       <Button theme="text" level="danger">危险文字按钮</Button>
     </div>
   </div>
-  <p>示例4</p>
+  <h1>示例4</h1>
   <div>
     <Button disabled>禁用按钮</Button>
     <Button theme="link" disabled>禁用链接按钮</Button>
     <Button theme="text" disabled>禁用按钮</Button>
+  </div>
+  <h1>示例5</h1>
+  <div>
+    <Button loading>加载中</Button>
   </div>
 </template>
 
